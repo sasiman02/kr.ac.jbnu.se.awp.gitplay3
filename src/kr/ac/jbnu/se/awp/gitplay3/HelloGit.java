87136d4d997a119b40cdef97fc8b1afa455a5cc8
sasiman02 <gustndlfrl@gmail.com>
2020-09-17 15:35:33 +0900

    # WARNING: head commit changed in the meantime
    이름 중복 제거

--- src/kr/ac/jbnu/se/awp/gitplay3/HelloGit.java
+++ src/kr/ac/jbnu/se/awp/gitplay3/HelloGit.java
@@ -4,7 +4,6 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
 		System.out.println("kim sang woo");
-		System.out.println("kim Hyun su");
 		System.out.println("kim jung hun");
 		System.out.println("kim hyun su");
 		System.out.println("kim Bo gyung");
