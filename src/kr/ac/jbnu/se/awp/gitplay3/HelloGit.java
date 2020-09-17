a92e844d1b948b73079fb2b6d4217a88f8ce4e81
junghunkim3 <71321621+junghunkim3@users.noreply.github.com>
2020-09-17 15:00:05 +0900

    jung hun add

--- src/kr/ac/jbnu/se/awp/gitplay3/HelloGit.java
+++ src/kr/ac/jbnu/se/awp/gitplay3/HelloGit.java
@@ -4,6 +4,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
 		System.out.println("kim sang woo");
+		System.out.println("kim Hyun su");
 		System.out.println("kim jung hun");
 	}
 }
