44fa6821c02ede82a4a9bfcf1dcc577dfbbab8ed
Kim Hyunsu <38347891+sasiman02@users.noreply.github.com>
2020-09-17 14:53:26 +0900

    Update HelloGit.java

--- src/kr/ac/jbnu/se/awp/gitplay3/HelloGit.java
+++ src/kr/ac/jbnu/se/awp/gitplay3/HelloGit.java
@@ -3,6 +3,7 @@ package kr.ac.jbnu.se.awp.gitplay3;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
-		System.out.printlm("kim sang woo");
+		System.out.println("kim sang woo");
+		System.out.println("kim Hyun su");
 	}
 }
